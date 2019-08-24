8b82711feaf90ad06410fb3f72fab39c6c2d32a3
Maxime Laudrin <maxime.laudrin@protonmail.com>
2019-08-24 13:13:09 +0200

    Activate navigation for sidebar links, close sidebar on navigation in handset mode.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map, share } from 'rxjs/operators';
+import { map, share, withLatestFrom, filter } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'nav',
@@ -10,12 +12,20 @@ import { map, share } from 'rxjs/operators';
 })
 export class NavComponent {
 
+  @ViewChild('drawer', { static: true }) drawer: MatSidenav;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       share()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, router: Router) {
+    // Close sidenav drawer when user clicks on nav link (only is hanset mode).
+    router.events.pipe(
+      withLatestFrom(this.isHandset$),
+      filter(([a, b]) => b && a instanceof NavigationEnd)
+    ).subscribe(_ => this.drawer.close());
+  }
 
 }
